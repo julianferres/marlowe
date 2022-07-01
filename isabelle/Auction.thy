d50b25d2bbcea3e85f6f850a7a73dcd1eefd0478
julianferres <julianferres@gmail.com>
2022-07-01 11:40:08 -0300

    Fix base case definition of contractLoop

--- isabelle/Auction.thy
+++ isabelle/Auction.thy
@@ -65,6 +65,7 @@ where
                                   (contractLoop m ps' qs terms))))" |
 
 "contractLoop m [] [] terms = settle None terms" |
+"contractLoop m [] [] terms = settle m terms" |
 "contractLoop m ps qs terms = (When ( (map (handleChoose m ps qs terms) qs) @ 
                                       (map (handleDeposit m ps qs terms) ps)) 
                                       (deadline terms) (settle m terms))"  
